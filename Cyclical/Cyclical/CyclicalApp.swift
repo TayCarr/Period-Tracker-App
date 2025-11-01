8d91bc98077c3347097a0069ed6596a6b61b2935
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-10-31 18:00:11 -0600

    init

---
+++ Cyclical/Cyclical/CyclicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclicalApp.swift
+//  Cyclical
+//
+//  Created by Taylor Carreiro on 2025-10-31.
+//
+
+import SwiftUI
+
+@main
+struct CyclicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
